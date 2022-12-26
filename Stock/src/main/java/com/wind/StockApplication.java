5982f77e5fbefc970f06c4235ccb7e5971649043
windmaxiao <windxiao@vip.qq.com>
2022-12-26 20:57:44 +0800

    first commit

---
+++ Stock/src/main/java/com/wind/StockApplication.java
@@ -0,0 +1,15 @@
+package com.wind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author maxiao
+ */
+@SpringBootApplication
+public class StockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockApplication.class);
+    }
+}
